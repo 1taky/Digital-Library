10b0323374094772d21202311f8ad730d61e1ef5
1taky <20vitaliys06@gmail.com>
2026-06-04 23:39:12 +0300

    fix: fixed bugs with book dto and model

--- backend/DigitalLibrary.API/Models/Response/BookResponseModel.cs
+++ backend/DigitalLibrary.API/Models/Response/BookResponseModel.cs
@@ -12,8 +12,6 @@ public class BookResponseModel
 
     public string BookType { get; set; } = string.Empty;
 
-    public int GenreId { get; set; }
-
     public string GenreName { get; set; } = string.Empty;
 
     public string Language { get; set; } = string.Empty;
