74a49db5040375328e49269c7278a95af89465f9
announcerf <tasaneono22@gmail.com>
2026-06-05 19:19:34 +0300

    feat(web): create homepage layout

--- frontend/src/views/home/HomePage.vue
+++ frontend/src/views/home/HomePage.vue
@@ -1,41 +1,18 @@
 <script setup lang="ts">
-import { useAuthStore } from '@/app/stores';
 import { Container } from '@/shared/ui/container';
 import { Section } from '@/shared/ui/section';
 import { Typography } from '@/shared/ui/typography';
-import { useRouter } from 'vue-router';
+import { useBooks } from './model/use-books';
 
-const authStore = useAuthStore();
-const router = useRouter();
-
-const handleLogout = () => {
-  authStore.logout();
-  router.push('/');
-};
-const goToRegister = () => {
-  router.push('/register');
-};
-const goToLogin = () => {
-  router.push('/log-in');
-};
+const { books } = useBooks();
 </script>
 
 <template>
   <Section class-name="mt-20">
     <Container>
       <Typography as="h1">Головна сторінка</Typography>
-
-      <div v-if="authStore.user">
-        <p>Привіт, {{ authStore.user.fullName }}!</p>
-        <button @click="handleLogout">Вийти</button>
-      </div>
-
-      <div v-else>
-        <p>Ви не увійшли в систему.</p>
-        <button @click="goToRegister">Реєстрація</button>
-
-        <button @click="goToLogin" style="margin-left: 10px">Увійти</button>
-      </div>
+      <div v-for="book in books" genre="Хорор">{{ book.title }}</div>
+      <div v-for="book in books" genre="Драма">{{ book.title }}</div>
     </Container>
   </Section>
 </template>
